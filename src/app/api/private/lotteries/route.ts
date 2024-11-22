7faea5cbd90b208be3b9d996c0718b280ca2e12b
penalba <carlos@getxplor.com>
2024-11-21 22:55:41 -0500

    console.error: get lottery data

--- src/app/api/private/lotteries/route.ts
+++ src/app/api/private/lotteries/route.ts
@@ -1,6 +1,9 @@
 import { getLotteryData } from "./schedule";
 
 export async function POST() {
-  await getLotteryData().catch(() => console.error("Error: Get Lottery Data"));
+  await getLotteryData().catch((e) => {
+    console.error("Error: Get Lottery Data");
+    console.error(e);
+  });
   return Response.json({ message: "Scheduled Task!" });
 }
