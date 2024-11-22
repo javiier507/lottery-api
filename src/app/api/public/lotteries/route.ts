92a5f9e7067ae4ffea919c4026a9768919199054
penalba <carlos@getxplor.com>
2024-11-21 21:39:41 -0500

    fix: console.error

--- src/app/api/public/lotteries/route.ts
+++ src/app/api/public/lotteries/route.ts
@@ -1,24 +1,25 @@
-import { type NextRequest } from 'next/server';
-import { z } from 'zod';
+import { type NextRequest } from "next/server";
+import { z } from "zod";
 
-import {
-  getLotteries
-} from "@/db/repositories/lottery.repository";
+import { getLotteries } from "@/db/repositories/lottery.repository";
 
 const paginationSchema = z.object({
-  limit: z.coerce.number().default(10).transform(x => x < 10 ? 10 : x),
-  offset: z.coerce.number().default(0)
-})
+  limit: z.coerce
+    .number()
+    .default(10)
+    .transform((x) => (x < 10 ? 10 : x)),
+  offset: z.coerce.number().default(0),
+});
 
 export async function GET(request: NextRequest) {
   const queryParams = request.nextUrl.searchParams;
 
   const paginationParams = {
-    limit: queryParams.get('limit'),
-    offset: queryParams.get('offset')
-  }
+    limit: queryParams.get("limit"),
+    offset: queryParams.get("offset"),
+  };
   const parsed = paginationSchema.safeParse(paginationParams);
-  if(!parsed.success) {
+  if (!parsed.success) {
     return Response.json(
       { message: "The parameters do not comply with the correct format" },
       { status: 400 }
@@ -30,6 +31,8 @@ export async function GET(request: NextRequest) {
       return Response.json({ data });
     })
     .catch((e) => {
+      console.error("Error: Get Lotteries");
+      console.error(e);
       return Response.json(
         { message: "Sorry, something went wrong" },
         { status: 500 }
