e893fdae6b6c1d754966430a9de8eb74b8be1cc9
penalbaxplor <carlos@getxplor.com>
2025-02-22 22:47:38 -0500

    feat: obteniendo loterias de la bd

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -1,27 +1,23 @@
-import { LotteryTableWrapper } from "@/components/lottery-table-wrapper";
-import { Container } from "@/components/container";
 import Link from "next/link";
 
-export default function ManagementPage() {
-  return (
-    <Container>
-      <Link 
-        href="/management/new" 
-        className="mb-4 inline-block bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition-colors"
-      >
-        Create New Lottery
-      </Link>
-      <LotteryTableWrapper lotteries={[
-        //generate 20 lotteries
-        ...Array.from({ length: 20 }, (_, index) => ({
-            draw: `Draw ${index + 1}`,
-            dateTitle: `Date Title ${index + 1}`,
-            date: `Date ${index + 1}`,
-            firstPrize: `First Prize ${index + 1}`,
-            secondPrize: `Second Prize ${index + 1}`,
-            thirdPrize: `Third Prize ${index + 1}`,
-        }))
-    ]} />
-    </Container>
-  );
+import { LotteryTableWrapper } from "@/components/lottery-table-wrapper";
+import { Container } from "@/components/container";
+
+import { getLotteries } from "@/db/repositories/lottery.repository";
+
+export const dynamic = 'force-dynamic';
+
+export default async function ManagementPage() {
+    const lotteries = await getLotteries();
+    return (
+        <Container>
+            <Link 
+                href="/management/new" 
+                className="mb-4 inline-block bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition-colors"
+            >
+                Create New Lottery
+            </Link>
+            <LotteryTableWrapper lotteries={lotteries.records} />
+        </Container>
+    );
 }
