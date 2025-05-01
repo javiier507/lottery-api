d38bc6e60224c5f5a0224038ce2e900c40f1400e
penalbaxplor <carlos@getxplor.com>
2025-04-30 21:58:44 -0500

    refactor: mejorando legibilidad de codigo de cache max age

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -10,6 +10,14 @@ type MaxAge = {
 	seconds: number;
 };
 
+function getLimitDate(currentDate: Date) {
+	const limitDate = new Date(currentDate.toString());
+	limitDate.setUTCHours(20);
+	limitDate.setUTCMinutes(30);
+	limitDate.setUTCMilliseconds(0);
+	return limitDate;
+}
+
 export function getMaxAge(currentDate: Date): MaxAge {
 	currentDate.setUTCMilliseconds(0);
 	const hour = currentDate.getUTCHours();
@@ -17,18 +25,36 @@ export function getMaxAge(currentDate: Date): MaxAge {
 
 	let expirationDate = new Date(currentDate.toString());
 
-	const limitDate = new Date(currentDate.toString());
-	limitDate.setUTCHours(20);
-	limitDate.setUTCMinutes(30);
-	limitDate.setUTCMilliseconds(0);
-
-	if ((hour === 20 && minutes > 30) || (hour === 21 && minutes < 30)) {
-		expirationDate = addMinutes(expirationDate, 2);
-	} else if (hour >= 21) {
-		expirationDate = addMinutes(expirationDate, 60);
-	} else {
-		const diffMinutes = calculateMinutesBetweenDates(expirationDate, limitDate);
+	const setToLimitDate = () => {
+		const diffMinutes = calculateMinutesBetweenDates(
+			expirationDate,
+			getLimitDate(currentDate),
+		);
 		expirationDate = addMinutes(expirationDate, diffMinutes);
+	};
+
+	switch (true) {
+		case hour <= 18:
+			expirationDate = addMinutes(expirationDate, 60);
+			break;
+		case hour === 19:
+			setToLimitDate();
+			break;
+		case hour === 20 && minutes < 30:
+			setToLimitDate();
+			break;
+		case hour === 20 && minutes >= 30:
+			expirationDate = addMinutes(expirationDate, 2);
+			break;
+		case hour === 21 && minutes < 30:
+			expirationDate = addMinutes(expirationDate, 2);
+			break;
+		case hour === 21 && minutes >= 30:
+			expirationDate = addMinutes(expirationDate, 60);
+			break;
+		case hour >= 22:
+			expirationDate = addMinutes(expirationDate, 60);
+			break;
 	}
 
 	const seconds = calculateSecondsBetweenDates(currentDate, expirationDate);
