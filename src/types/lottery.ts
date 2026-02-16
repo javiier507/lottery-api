011826088959e787c8aadd46a1b4836f23499ea4
penalbaxplor <carlos@getxplor.com>
2026-02-16 13:03:44 -0500

    feat: getting kind lottery from telemetro

--- src/types/lottery.ts
+++ src/types/lottery.ts
@@ -1,3 +1,12 @@
+export type Kind = 1 | 2 | 3 | 4;
+
+export const KindMap: Record<string, Kind> = {
+	dominical: 1,
+	miercolito: 2,
+	gordito: 3,
+	extraordinario: 4,
+} as const;
+
 export type Lottery = {
 	draw: string;
 	dateTitle: string;
@@ -8,4 +17,5 @@ export type Lottery = {
 	letters?: string | null;
 	serie?: string | null;
 	folio?: string | null;
+	kind?: Kind;
 };
